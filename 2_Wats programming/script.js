362248c0eaf932931c265f94b16547be24e1a65b
Taha Daneshi <mttahadaneshi1384mt@gmail.com>
2025-11-03 02:14:25 +0330

    completed lesson 2

--- 2_Wats programming/script.js
+++ 2_Wats programming/script.js
@@ -1,9 +1,10 @@
-import headerOne, { solidParagraph } from '../Auto_Create_Pages.js';
+import headerOne, { solidParagraph, definitionParagraph, pointParagraph } from '../Auto_Create_Pages.js';
 const body = document.getElementById('body');
+const h1 = ['برنامه نویسی چسیت؟', 'Whats Programming?']
 const paragraphs = {
-  starter: ['تازمانی که وارد کار برنامه نویسی نشده اید نمی نوانید به معنای رقیقش پی ببرید', ''],
+  starter: ['تازمانی که وارد کار برنامه نویسی نشده اید نمی توانید به معنای واقعی ان پی ببرید', ''],
   theGeneralConceptOfProgramming: ['مفهوم کلی برنامه نویسی اینه که برنامه ای را بنویسید.', ''],
-  definitionOfProgramming: ['اجرای دستورالعمل هایی که مرتب اجرامیشن و یک خروجی به ما میدهند.', ""],
+  definitionOfProgramming: ['تعریف برنامه نویسی: اجرای دستورالعمل هایی که مرتب اجرامیشن و یک خروجی به ما میدهند.', ""],
   pointOfProgrammingIsPrecise: ['نکته: برنامه باید خیلی دقیق باشد چون که کامپیوتر و سخت افزارها هوش ندارند و فقط کاری که ما گفتیم انجام میدهند نه کمتر و نه بیشتر.', ''],
   teaMaker: [
     "چای ساز یه برنامه براش نوشته شده که تازمانی که دما آب به این مقدار نرسیده گرم میشود و بعد از اینکه مثلا به دمای ۱۰۰ درجه رسید دیگه گرمتر نمیشه و زمانی که دماش به ۸۵ درجه رسید دوباره گرم میشود.", ''],
@@ -11,7 +12,14 @@ const paragraphs = {
   mostElectricalAppliances: ['دی دنیای واقعی اکثر مواردی که با برق کار میکنند از تکنولوژی های مختلف برنامه نویسی استفاده میکنند مثل: کامپیوتر و ماشین و وسایل آشپزخانه برقی و پزشکی و هوافضا ...', '']
 }
 function Fa() {
-
+  headerOne(body, h1[0]);
+  solidParagraph(body, paragraphs.starter[0]);
+  solidParagraph(body, paragraphs.theGeneralConceptOfProgramming[0]);
+  definitionParagraph(body, paragraphs.definitionOfProgramming[0]);
+  pointParagraph(body, paragraphs.pointOfProgrammingIsPrecise[0]);
+  solidParagraph(body, paragraphs.teaMaker[0]);
+  solidParagraph(body, paragraphs.washingMachine[0]);
+  solidParagraph(body, mostElectricalAppliances[0]);
 };
 Fa();
 function En() {
