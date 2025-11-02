519916eab34a713d78cbf52a54110e65c9dc5b29
Taha Daneshi <mttahadaneshi1384mt@gmail.com>
2025-11-02 19:54:00 +0330

    complete Auto_Create_Pages frome lesson_1

--- Auto_Create_Pages.js
+++ Auto_Create_Pages.js
@@ -1,12 +1,19 @@
 // Frame work
-const place = document.getElementById('taha')
-function header(place, text) {
-  place.insertAdjacentHTML('beforeend', `<h1>${text}<h1>`)
+export default function headerOne(place, text) {
+  place.insertAdjacentHTML('beforeend', `<h1>${text}</h1>`)
 }
-function unorderedList(place, text) {
+export function solidParagraph(place, text) {
   place.insertAdjacentHTML('beforeend', `<p class="paragraph">${text}<p>`)
 }
-module.exports = {
-  header,
-  unorderedList
+export function solidUnorderedList(place, title = 'sring', array = []) {
+  let li = '';
+  array.forEach(element => {
+    li += `<li>${element}</li>`
+  });
+  place.insertAdjacentHTML("beforeend", `
+    <ul>
+      <h2>${title}</h2>
+      ${li}
+    </ul>`);
 }
+// EVENTS
