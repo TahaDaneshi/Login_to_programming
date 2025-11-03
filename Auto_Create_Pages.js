6ce17af1ccb6f5de54b228c3fbe21f0830b1131d
Taha Daneshi <mttahadaneshi1384mt@gmail.com>
2025-11-03 05:00:40 +0330

    added function frome create heade tage

--- Auto_Create_Pages.js
+++ Auto_Create_Pages.js
@@ -2,9 +2,20 @@
 export default function headerOne(place, text) {
   place.insertAdjacentHTML('beforeend', `<h1>${text}</h1>`)
 }
+export function head(html, title = 'string') {
+  html.insertAdjacentHTML('afterstart', `
+    <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>${title}</title>
+  <link rel="stylesheet" href="../style.css">
+  <script type="module" defer src="./script.js"></script>
+  </head>
+    `)
+};
 export function solidParagraph(place, text) {
   place.insertAdjacentHTML('beforeend', `<p class="paragraph">${text}<p>`)
-}
+};
 export function solidUnorderedList(place, title = 'sring', array = []) {
   let li = '';
   array.forEach(element => {
@@ -15,15 +26,15 @@ export function solidUnorderedList(place, title = 'sring', array = []) {
       <h2>${title}</h2>
       ${li}
     </ul>`);
-}
+};
 export function definitionParagraph(place, text = 'string') {
   place.insertAdjacentHTML('beforeend', `
     <p class="definition">${text}</p>
     `)
-}
+};
 export function pointParagraph(place, text = 'string') {
   place.insertAdjacentHTML('beforeend', `
     <p class='point'>${text}</p>
     `)
-}
+};
 // EVENTS
