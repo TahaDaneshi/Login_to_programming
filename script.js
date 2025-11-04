85fdac35c474eca19e0ab3701e3657392aa42e86
Taha Daneshi <mttahadaneshi1384mt@gmail.com>
2025-11-04 22:11:07 +0330

    imporve structure main page

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const LESSONS = [
-  { lesson: 1, name: "مقدمه", Date: "1404/08/11", href: './1_Intro/lesson_1.html' }
+  { FaName: "مقدمه", EnName: '', FaDate: "1404/08/11", href: './1_Intro/lesson_1.html' }
 ]
+const html = document.documentElement;
 const body = document.getElementsByTagName('body')
-
-LESSONS.forEach(element => {
-  body[0].insertAdjacentHTML('beforeend', `<a href="${element.href}" target="_blank">
+function mainPage(language = 'Fa') {
+  LESSONS.forEach((element, index) => {
+    let Fa = { name: element.FaName, date: element.FaDate }
+    let En = { name: element.EnName, date: element.FaDate }
+    let lang = language === 'Fa' ? Fa : En;
+    body[0].insertAdjacentHTML('beforeend', `<a href="${element.href}" target="_blank">
     <div>
-      <h2>${element.lesson}: ${element.name}</h2>
-      <p>${element.Date}</p>
+      <h2>${index++}: ${lang.name}</h2>
+      <p>${lang.date}</p>
     </div>
   </a>`)
-});
+  })
+};
+mainPage('Fa');
