0bbb561c2f3e21456f5eacf8553a658cf94eb762
Taha Daneshi <mttahadaneshi1384mt@gmail.com>
2025-11-04 23:53:59 +0330

    Complete lesson 8 base.

---
+++ 8_Programming_Reality/script.js
@@ -0,0 +1,17 @@
+import header, { head, paragraph, solidUnorderedList } from '../Auto_Create_Pages.js';
+const html = document.documentElement;
+head(html, 'Programming Reality');
+const body = document.getElementById('body');
+const title = ["واقعیت های دنیای برنامه نویسی", "Programming Reality"];
+
+const ul = {
+  title: ["واقعیت های برنامه نویسی:", "Programming Reality"], texts: [[["صبر و پشتکار:", "برنامه نویسی صبر و پشتکار زیادی نیاز دارد چونکه قراره در روند یادگیری و کارت کلی مشکل توی پروژه هات به وجود بیاد."], ["آزمون و خطا:", "برای حل کردن مشکلات پرژت آزمون و خطاهای زیادی باید انجام دهید تا مشکل برترف شود."], ["سبک زندگی:", "نمیشه به عنوان شغل دوم به برنامه نویسی نگاه کرد چون که پیچیدگی و گستردگی زیادی هست و نمیشه تفریحی رفت سمتش."]],
+  [["", ""], ["", ""], ["", ""]]]
+};
+const attention = ["بنامه نویسی یک شغل است مثل باقی شغل ها و خیلی عجیب و غریب نیست.", ""];
+const goldenSentence = ["باد آورده را باد میبرد: هیچع سخت تر چیزی را به دست بیاوری سخت تر ازدست میدهی.", ""];
+const ulBook = {
+  title: ['کتاب های توسعه فردی:', ''], bookNames: [['اثرمرکب', 'ترزفکر', 'کار عمیق', 'بی حدومرز', 'چگونه کمالگرا نباشیم'],
+  ['', '', '', '', '']]
+};
+const instagramAddress = ["ادرس اینستاگرام معلم: ebadi.dev", "Instagram address: ebadi.dev"];
