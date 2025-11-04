a329ba1f2e5cd4bc42abe4f23a30d48a3ef3622e
Taha Daneshi <mttahadaneshi1384mt@gmail.com>
2025-11-04 20:12:31 +0330

    Lesson 6 container ready

--- 6_How_to_Learn/script.js
+++ 6_How_to_Learn/script.js
@@ -1,4 +1,23 @@
 import header, { head, paragraph, solidUnorderedList } from '../Auto_Create_Pages.js';
 const html = document.documentElement;
-head(html, 'coding career');
+head(html, 'How to Learn?');
 const body = document.getElementById('body');
+const title = ["مدت زمان مورد نیاز برای یادگیری و روش های یادگیری؟", 'How to Learn?'];
+const texts = [{
+  time: { header: '', paragraph: '', attention: '' },
+  bestMethod: { header: '', p: '' },
+  typeOfCourse: { header: '', p: '' },
+  hour: { header: '', p1: '', p2: '' },
+  whatShouldLearn: { header: '', link: '', p: '' }
+}, {
+  time: { header: '', paragraph: '', attention: '' },
+  bestMethod: { header: '', p: '' },
+  typeOfCourse: { header: '', p: '' },
+  hour: { header: '', p1: '', p2: '' },
+  whatShouldLearn: { header: '', link: '', p: '' }
+}];
+// FUNCTIONS
+function Page() {
+
+};
+Page();
