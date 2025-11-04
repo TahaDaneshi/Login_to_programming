86bbb09bb91b6ba8e5635c56e54194260066e5b0
Taha Daneshi <mttahadaneshi1384mt@gmail.com>
2025-11-04 19:56:26 +0330

    directory frome lesson 6 is create.

---
+++ 6_How_to_Learn/script.js
@@ -0,0 +1,4 @@
+import header, { head, paragraph, solidUnorderedList } from '../Auto_Create_Pages.js';
+const html = document.documentElement;
+head(html, 'coding career');
+const body = document.getElementById('body');
