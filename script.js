a6c17cbc3a663a4d22a40f4dc8ea47519b177403
Taha Daneshi <mttahadaneshi1384mt@gmail.com>
2025-11-05 02:33:04 +0330

    complete main Page

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
 const html = document.documentElement;
-const body = document.getElementsByTagName('body')[0];
+const body = document.getElementById('body');
 const Lessons = [
-  { Name: ["مقدمه", 'Intro'], Date: "1404/08/11" },
-  { Name: ["برنامه نویسی چیست؟", "Whats Programming"], Date: "1404/08/11" },
-  { Name: ["شاخه های برنامه نویسی", "Programming Branches"], Date: "1404/08/12" },
-  { Name: ["ارتباط ریاضی و زبان انگلیسی در یادگیری", "Math and English"], Date: "1404/08/13" },
-  { Name: ["", "coding career"], Date: "1404/08/13" },
-  { Name: ["", "How to Learn"], Date: "1404/08/13" },
-  { Name: ["", "Research and Choose Course"], Date: "1404/08/13" },
-  { Name: ["", "Programming Reality"], Date: "1404/08/15" },
-  { Name: ["", "Big Mistake University"], Date: "1404/08/15" }
+  {
+    Name: ["مقدمه", 'Intro'],
+    Date: "1404/08/11"
+  },
+  {
+    Name: ["برنامه نویسی چیست؟", "Whats Programming"],
+    Date: "1404/08/11"
+  },
+  {
+    Name: ["شاخه های برنامه نویسی", "Programming Branches"],
+    Date: "1404/08/12"
+  },
+  {
+    Name: ["ارتباط ریاضی و زبان انگلیسی در یادگیری", "Math and English"],
+    Date: "1404/08/13"
+  },
+  {
+    Name: ["آینده شغلی برنامه نویسی و مسیرهای درآمدزایی", "coding career"],
+    Date: "1404/08/13"
+  },
+  {
+    Name: ["مدت زمان مورد نیاز برای یادگیری و روش های یادگیری", "How to Learn"],
+    Date: "1404/08/13"
+  },
+  {
+    Name: ["دوره آموزشی که میبینیم چه ویژگی هایی داشته باشه؟ ریسرچربودن چیه؟", "Research and Choose Course"],
+    Date: "1404/08/13"
+  },
+  {
+    Name: ["واقعیت های ناگفته دنیای برنامه نویسی", "Programming Reality"],
+    Date: "1404/08/15"
+  },
+  {
+    Name: ["بزرگ ترین اشتباح تازه کارها! دانشگاه بریم یا نریم؟", "Big Mistake University"],
+    Date: "1404/08/15"
+  }
 ];
 let href = [];
-/*
-  <a>
-    <div>
-      <h2>${index++}: ${lang.name}</h2>
-      <p>${lang.date}</p>
-    </div>
-  </a>
-*/
 function Page(language) {
   let lang = language === 'Fa' ? 0 : 1;
+  Lessons.forEach((element, index) => {
+    let test = element.Name[1].replaceAll(' ', '_');
+    href.push();
+    body.insertAdjacentHTML('beforeend', `
+    <a href="./${++index}_${test}/${test}.html">
+    <div>
+      <h2>${index}: ${element.Name[lang]}</h2>
+      <p>${element.Date}</p>
+    </div>
+  </a>`)
+  });
 };
 Page('Fa');
